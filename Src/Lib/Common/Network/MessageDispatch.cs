7ae6a2b214ce59f08e242d86a9949709c0ba169e
Traveler-Yang <1187095757@qq.com>
2025-10-11 22:11:45 +0800

    协议修改

--- Src/Lib/Common/Network/MessageDispatch.cs
+++ Src/Lib/Common/Network/MessageDispatch.cs
@@ -25,6 +25,18 @@ namespace Network
             if (message.friendList != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.friendList); }
             if (message.friendRemoveRes != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.friendRemoveRes); }
             if (message.friendRemoveNotify != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.friendRemoveNotify); }
+            if (message.tempInviteRes != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.tempInviteRes); }
+            if (message.tempInfo != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.tempInfo); }
+            if (message.TempLeave != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.TempLeave); }
+            if (message.guildCreat != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildCreat); }
+            if (message.guildJoinReq != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildJoinReq); }
+            if (message.guildJoinRes != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildJoinRes); }
+            if (message.Guild != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.Guild); }
+            if (message.guildLeave != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildLeave); }
+            if (message.guildList != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildList); }
+            if (message.guildSearch != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildSearch); }
+            if (message.guildAdmin != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildAdmin); }
+            if (message.Chat != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.Chat); }
 
         }
 
@@ -46,6 +58,19 @@ namespace Network
             if (message.friendAddRes != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.friendAddRes); }
             if (message.friendList != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.friendList); }
             if (message.friendRemoveReq != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.friendRemoveReq); }
+            if (message.tempInviteReq != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.tempInviteReq); }
+            if (message.tempInviteRes != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.tempInviteRes); }
+            if (message.tempInfo != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.tempInfo); }
+            if (message.TempLeave != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.TempLeave); }
+            if (message.guildCreat != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildCreat); }
+            if (message.guildJoinReq != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildJoinReq); }
+            if (message.guildJoinRes != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildJoinRes); }
+            if (message.Guild != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.Guild); }
+            if (message.guildLeave != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildLeave); }
+            if (message.guildList != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildList); }
+            if (message.guildSearch != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildSearch); }
+            if (message.guildAdmin != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.guildAdmin); }
+            if (message.Chat != null) { MessageDistributer<T>.Instance.RaiseEvent(sender, message.Chat); }
 
         }
     }
